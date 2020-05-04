adf47072a8044e8230e9f0e76a22ee5dc03f5ae3
lanzimauro <lanzi_mauro@yahoo.it>
2020-05-04 17:44:11 +0200

    creazione card divise per categoria

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel-primi-passi</title>
+    </head>
+    <body>
+        <main>
+            <a href="{{route('home')}}">Home</a>
+        </main>
+    </body>
+</html>
